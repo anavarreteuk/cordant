001369983c55cb675364bb368f8599eb5280834e
anavarreteuk <a.navarrete.uk@gmail.com>
2019-03-06 01:17:10 +0000

    tyding up

--- src/Photo.js
+++ src/Photo.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React from "react";
 import { Col, Card } from "react-bootstrap";
 
-
-const Photo = (props) => {
+const Photo = props => {
   return (
-      <Col sm={3}>
-          <Card style={{ width: "10rem" }}>
-              <Card.Img src={props.photoItem.url} />
-          </Card>
-      </Col>
-  )
-}
+    <Col sm={3}>
+      <Card style={{ width: "10rem" }}>
+        <Card.Img src={props.photoItem.url} />
+      </Card>
+    </Col>
+  );
+};
 
-export default Photo
+export default Photo;
